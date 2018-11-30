cc67c2fcd0301cee06383567c075f98bad0dc161
JavyHuerta <javyhc95@gmail.com>
2018-11-30 12:06:41 -0800

    Se agregaron los archivos maestro y esclavo de arduino

---
+++ esclavo.ino
@@ -0,0 +1,70 @@
+﻿#include <Wire.h>
+#include <Stepper.h>
+
+// Motor 1
+int m1In1 = 4;
+int m1In2 = 5;
+int m1In3 = 6;
+int m1In4 = 7;
+Stepper motor1(2048,m1In1, m1In3, m1In2, m1In4);
+
+//Motor 2
+int m2In1 = 8;
+int m2In2 = 9;
+int m2In3 = 10;
+int m2In4 = 11;
+Stepper motor2(2048,m2In1, m2In3, m2In2, m2In4);
+
+
+int x; // Variable para el evento
+void setup()
+{
+  motor1.setSpeed(3);
+  motor2.setSpeed(3);
+  Wire.begin(1);
+  Wire.onReceive(recibidoEvento); 
+  Serial.begin(9600);
+  MoverM1();
+ 
+}
+
+
+void loop()
+{
+ delay(2500);
+ if(x==1){
+  MoverM1();
+  x=0;
+  }
+
+  if(x==2){
+  MoverM2();
+  x=0;
+  }
+ 
+}
+
+void recibidoEvento(int recepcion)
+  {
+    while(Wire.available()) 
+    {
+      x = Wire.read(); 
+    }
+
+  }
+
+  void MoverM1(){
+    motor1.step(2048);
+    digitalWrite(m1In1, LOW);
+    digitalWrite(m1In2, LOW);
+    digitalWrite(m1In3, LOW);
+    digitalWrite(m1In4, LOW);
+  }
+
+  void MoverM2(){
+    motor2.step(-2048);
+    digitalWrite(m2In1, LOW);
+    digitalWrite(m2In2, LOW);
+    digitalWrite(m2In3, LOW);
+    digitalWrite(m2In4, LOW);
+  }
---
+++ maestro.ino
@@ -0,0 +1,72 @@
+﻿#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Wire.h>
+
+
+const char* ssid = "Nombre de la red";
+const char* password = "Contraseña  de la red";
+WiFiServer server(80);
+
+int motor = 0;
+
+void setup() {
+  Wire.begin(); // 
+   Serial.begin(115200);
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+ 
+    delay(1000);
+    Serial.print("Connecting..");
+ 
+  }
+}
+ 
+void loop() {
+ 
+  delay(2000);
+  if (WiFi.status() == WL_CONNECTED) { 
+ 
+    HTTPClient http; 
+    
+    http.begin("http://api.thingspeak.com/channels/< your api key>/feeds/last.json");
+    int httpCode = http.GET();                                                       
+ 
+    if (httpCode > 0) { 
+      DynamicJsonBuffer jsonBuffer;
+      String  payload = http.getString();
+       JsonObject& root = jsonBuffer.parseObject(payload);      
+      String motor= root["field1"];
+      http.end();
+  
+      if(motor == "motor1"){
+         Wire.beginTransmission(1); 
+      Wire.write(1); 
+      Wire.endTransmission();
+        InsertNone();
+        motor="0";
+      }
+      if(motor == "motor2"){
+        Wire.beginTransmission(1);
+      Wire.write(2); 
+      Wire.endTransmission();
+        InsertNone();
+        motor = "0";
+      }
+    }
+  }
+ 
+  delay(3000);   
+ 
+}
+
+void InsertNone(){
+   // Insertar vacio
+   HTTPClient httpDel;
+   httpDel.begin("http://api.thingspeak.com/update.json?api_key=<your api key>&field1=0");
+   int httpCode2 = httpDel.POST("Enviado desde arduino");    
+   String pay = httpDel.getString();
+   httpDel.end();
+   delay(2000);
+}
